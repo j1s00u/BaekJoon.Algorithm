961622610fc0f9a6e7b8bc0d2534e84d880131af
jisoo <flzk7920@gmail.com>
2017-09-02 16:02:06 +0900

    [BaekJoon] 스택 사용하기

--- src/com/java/stack/No_10828.java
+++ src/com/java/stack/No_10828.java
@@ -1,10 +1,78 @@
 package com.java.stack;
 
+import java.util.Scanner;
+
 public class No_10828 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stack stack = new Stack();
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		String cmd;
 
+		for (int i = 1; i <= input; i++) {
+			cmd = scan.next();
+			if (cmd.equals("push")) {
+				int x = scan.nextInt();
+				stack.push(x);
+			} else if (cmd.equals("pop")) {
+				stack.pop();
+			} else if (cmd.equals("size")) {
+				stack.size();
+			} else if (cmd.equals("empty")) {
+				stack.empty();
+			} else if (cmd.equals("top")) {
+				stack.top();
+			}
+		}
+	}
+
+	static class Stack {
+		private int size = 0;
+		private SNode top = null;
+
+		private class SNode {
+			int data;
+			private SNode next;
+		}
+
+		public void top() {
+			if (top == null) {
+				System.out.println("-1");
+			} else {
+				System.out.println(top.data);
+			}
+		}
+
+		public void empty() {
+			if (size == 0) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		}
+
+		public void size() {
+			System.out.println(size);
+		}
+
+		public void push(int x) {
+			SNode node = new SNode();
+			node.data = x;
+			node.next = top;
+			top = node;
+			size++;
+		}
+
+		public void pop() {
+			if (size == 0) {
+				System.out.println("-1");
+			} else {
+				System.out.println(top.data);
+				top = top.next;
+				size--;
+			}
+		}
 	}
 
 }
